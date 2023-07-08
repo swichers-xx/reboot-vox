afb7dadadc51f16ae0c0a503af49b89c5647903d
Your Name <you@example.com>
2023-07-08 01:49:51 -0600

    aider: Added error reporting and handling for subprocess calls in `app.py`.
    # Context:
    USER: Add error reporting and hanlding
    ASSISTANT: To add error reporting and handling, we need to modify the `app.py` file. We will add try-except blocks around the subprocess calls to catch any exceptions that might occur. If an exception occurs, we will log the error message and return a user-friendly error message.
    
    Here is the proposed change:
    
    ```python
    app.py
    <<<<<<< ORIGINAL
    import subprocess
    
    def get_service_status(server_name, service_name):
        # Run the command on the remote server and return the output
        return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service -Name {service_name} }}"])
    
    def get_service_list(server_name):
        # Run the command on the remote server and return the output
        return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service }}"])
    =======
    import subprocess
    import logging
    
    def get_service_status(server_name, service_name):
        try:
            # Run the command on the remote server and return the output
            return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service -Name {service_name} }}"])
        except Exception as e:
            logging.error(f"Error getting service status: {e}")
            return "Error getting service status"
    
    def get_service_list(server_name):
        try:
            # Run the command on the remote server and return the output
            return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service }}"])
        except Exception as e:
            logging.error(f"Error getting service list: {e}")
            return "Error getting service list"
    >>>>>>> UPDATED
    ```
    
    This change will catch any exceptions that occur when running the subprocess commands and log the error message. It will also return a user-friendly error message.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import subprocess
+import logging
 
 def get_service_status(server_name, service_name):
-    # Run the command on the remote server and return the output
-    return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service -Name {service_name} }}"])
+    try:
+        # Run the command on the remote server and return the output
+        return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service -Name {service_name} }}"])
+    except Exception as e:
+        logging.error(f"Error getting service status: {e}")
+        return "Error getting service status"
 
 def get_service_list(server_name):
-    # Run the command on the remote server and return the output
-    return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service }}"])
+    try:
+        # Run the command on the remote server and return the output
+        return subprocess.check_output(["powershell", "-Command", f"Invoke-Command -ComputerName {server_name} -ScriptBlock {{ Get-Service }}"])
+    except Exception as e:
+        logging.error(f"Error getting service list: {e}")
+        return "Error getting service list"
