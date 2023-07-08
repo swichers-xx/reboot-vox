27500267bb90e63d4e3619a2e603ef2af302d15b
swichers-xx <seanwichers@gmail.com>
2023-07-08 03:05:39 -0600

    Update exampleresource.py

--- app/resources/exampleresource.py
+++ app/resources/exampleresource.py
@@ -1,1 +1,15 @@
-# No changes needed
+from flask_restful import Resource
+import app
+
+class ExampleResource(Resource):
+    def get(self, server_name):
+        # Get the power status of the server
+        power_status = app.get_power_status(server_name)
+
+        return {'power_status': power_status}
+
+    def post(self, server_name):
+        # Toggle the power status of the server
+        app.toggle_power_status(server_name)
+
+        return {'message': f'Toggled power status of server {server_name}'}# No changes needed
