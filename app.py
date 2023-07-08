8dfbc9874970684afa6c01d85d45bced92aff3a5
swichers-xx <seanwichers@gmail.com>
2023-07-08 04:05:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ def get_service_list(server_name):
     except Exception as e:
         logging.error(f"Error getting service list: {e}")
         return "Error getting service list"
+        
+def get_status_for_list(server_service_list):
+    status_list = []
+    for server_name, service_name in server_service_list:
+        status = get_service_status(server_name, service_name)
+        status_list.append((server_name, service_name, status))
+    return status_list
